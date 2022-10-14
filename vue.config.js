550d99b5403dc51eb7daf8dc55557cb24cf9f9c5
Kevin Kössl <kevin.koessl@gmail.com>
2022-10-14 21:16:20 +0200

    Fixed dependencies for MacOS, Probably broke them for Ubuntu and Windows

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,21 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
     loaderOptions: {
       scss: {
+        sassOptions: {
+          includePaths: [path.resolve(__dirname, 'src/core/')],
+        },
         /*
           Important: When we refactor the css files and change the folder structure, we have to keep in mind that we have to
           change the injection here as well.
         */
-        prependData: `
+        additionalData: `
           @import "~bulma/sass/utilities/_all";
-          @import "~@/styles/abstracts/_style.scss";
+          @import "//styles/abstracts/_style.scss";
         `,
       },
     },
